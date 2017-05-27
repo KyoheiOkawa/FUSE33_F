42c5b1f9599382ad909334e0caa0328b754a907d
Takamichi Anzai <zeal15300180@c-league.jp>
2017-05-27 16:02:15 +0900

    文字の変えるやつ

---
+++ Assets/Anzai/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour {
+
+    public static int _LevelScore = 0;
+    public int a = 0;
+
+    void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Title")
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        a = _LevelScore;
+	}
+}
