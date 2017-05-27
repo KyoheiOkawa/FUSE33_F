024b64613024b107135751955e209f1fac85c8c3
yamaguchi <toru.yamaguchi109@gmail.com>
2017-05-27 16:58:29 +0900

    Title

---
+++ Assets/RenYamaguchi/TitleRobo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleRobo : MonoBehaviour {
+
+	void Update()
+	{
+		transform.Translate(Vector3.forward * Time.deltaTime);
+		transform.RotateAround(Vector3.up, 1 * Time.deltaTime);
+	}
+}
