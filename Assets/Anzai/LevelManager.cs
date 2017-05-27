a52c19729216cdbd787fad894d9dd4114eefe9e7
Takamichi Anzai <zeal15300180@c-league.jp>
2017-05-27 14:39:01 +0900

    ゴール　レベル

---
+++ Assets/Anzai/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+    static public int _GameLevel = 1;
+
+    public GameObject TextObj;
+    Text _myText;
+
+    // Use this for initialization
+    void Start()
+    {
+        _myText = TextObj.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _myText.text = "Level : " + _GameLevel.ToString();//テキストの変更
+
+    }
+}
