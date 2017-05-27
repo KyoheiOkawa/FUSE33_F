d2915ab376be804735086e0f38cc2f6af5983c0e
muroiyuki <zeal17300037@c-league.jp>
2017-05-27 16:12:35 +0900

    Title

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
@@ -15,6 +17,7 @@ public class Test : MonoBehaviour {
 	}
     public void OnClick()
     {
+        SceneManager.LoadScene("Stage1");
         Debug.Log("Call");
 
     }
